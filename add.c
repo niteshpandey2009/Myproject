443d07d4ce088ec183c6bd524864e6ef97b06482
niteshpandey2009 <niteshpandey2009@gmail.comm>
2026-03-11 23:39:24 +0530

    omkar name added

--- add.c
+++ add.c
@@ -3,11 +3,11 @@
 int main( )
 {
     int a,b;
-    printf("Enter number a\n");
+    printf("omkar Enter number a\n");
     scanf("%d",&a);
-    printf("Enter  number b\n ");
+    printf("omkar Enter  number b\n ");
     scanf("%d" , &b);
-    printf("The Sum of a and b =  %d\n", a+b);
+    printf("omkarThe Sum of a and b =  %d\n", a+b);
 
     return 0;
     
